f8a4d7187e96bd1815689b3a2411a89f26ed0e08
MyAsron <c.sic.3cdb@gmail.com>
2026-08-31 08:44:29 -0600

    Inicio del proyecto 1: Centro de Rescate Animal

---
+++ RescateAnimal/src/main/java/cris/sic/pryecto1/RescateAnimal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cris.sic.pryecto1;
+
+/**
+ *
+ * @author cris_sic
+ */
+public class RescateAnimal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
